0ee105c7e85d2c5b1b18f7644dea641f82d5c78a
pragnyaraog <gujjulapragnyarao@gmail.com>
2025-10-05 12:45:00 +0530

    first commit

---
+++ lengthofll.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class lengthofll {
+    Node head;
+
+    // Insert a new node at the end
+    void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Find length of the linked list (iterative)
+    int getLength() {
+        int count = 0;
+        Node current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        lengthofll list = new lengthofll();
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter data for node " + (i + 1) + ": ");
+            int data = sc.nextInt();
+            list.insert(data);
+        }
+
+        System.out.println("Length of linked list: " + list.getLength());
+        sc.close();
+    }
+}
+
